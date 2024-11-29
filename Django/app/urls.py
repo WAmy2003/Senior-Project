83751a9a8b235a2113f375fdcaf8c1ef0ee9cdcc
Amy <2400625@systex.com.tw>
2024-11-29 17:24:04 +0800

    修好127.0.0.1

--- Django/app/urls.py
+++ Django/app/urls.py
@@ -3,6 +3,10 @@ from . import views
 from .views import StockDataView
 
 urlpatterns = [
+    path('', views.main_view, name='main'),
+    path('portfolio.html', views.portfolio_view, name='portfolio'),
+    path('analysis.html', views.analysis_view, name='analysis'),
+    path('aboutus.html', views.aboutus_view, name='aboutus'),
     path('api/stock-data/', StockDataView.as_view(), name='stock-data'),
     path('api/portfolio-weights/', views.get_portfolio_weights, name='portfolio-weights'),
 ]
